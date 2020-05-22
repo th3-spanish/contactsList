5a6715cb82f9726f5ceed3ab705038d629206375
th3-spanish <vevomen1@gmail.com>
2020-05-22 06:30:37 +0100

    Updates

---
+++ precache-manifest.b9b368581448e3131049f3a83084bd6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fba2b179fbc01163bbaf8e0a2727705",
+    "url": "/contactsList/index.html"
+  },
+  {
+    "revision": "a8e95bed91a6d914320e",
+    "url": "/contactsList/static/css/main.a5fa40e9.chunk.css"
+  },
+  {
+    "revision": "1fe10fc989bab03faa56",
+    "url": "/contactsList/static/js/2.9bab3471.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contactsList/static/js/2.9bab3471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e95bed91a6d914320e",
+    "url": "/contactsList/static/js/main.6c847933.chunk.js"
+  },
+  {
+    "revision": "348d884c61562a6adadd",
+    "url": "/contactsList/static/js/runtime-main.b1eae48d.js"
+  }
+]);
